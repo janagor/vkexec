25b3b75fafc72a67329b97c32ae74e8db5b5fb39
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-09-17 20:19:55 +0900

    fix: include pass.hpp for groups_for in heap test

--- test/descriptor_heap_pipeline_tests.cpp
+++ test/descriptor_heap_pipeline_tests.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <vkexec/context.hpp>
+#include <vkexec/pass.hpp>
 #include <vkexec/pipeline.hpp>
 #include <vkexec/result.hpp>
 #include <vkexec/vulkan_requirements.hpp>
