bc12c6a826b11d595f18064275b6c2eac2adcd75
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-09-22 21:59:46 +0900

    fix: pass install config to install_consumer for multi-config CI

--- cmake/TestInstallConsumer.cmake
+++ cmake/TestInstallConsumer.cmake
@@ -30,10 +30,25 @@ set(prefix "${VKEXEC_BINARY_DIR}/install-consumer/prefix")
 set(build "${VKEXEC_BINARY_DIR}/install-consumer/build")
 set(parent_cache "${VKEXEC_BINARY_DIR}/CMakeCache.txt")
 
+# Multi-config generators place archives under $<CONFIG>/; cmake --install must
+# select the same configuration that ctest/-C and the parent build used.
+if(NOT DEFINED VKEXEC_CONFIG OR "${VKEXEC_CONFIG}" STREQUAL "" OR "${VKEXEC_CONFIG}" STREQUAL "$<CONFIG>")
+  vkexec_read_cache_entry("${parent_cache}" "CMAKE_BUILD_TYPE" _build_type)
+  vkexec_read_cache_entry("${parent_cache}" "CMAKE_DEFAULT_BUILD_TYPE" _default_build_type)
+  if(_build_type)
+    set(VKEXEC_CONFIG "${_build_type}")
+  elseif(_default_build_type)
+    set(VKEXEC_CONFIG "${_default_build_type}")
+  else()
+    set(VKEXEC_CONFIG "Release")
+  endif()
+endif()
+
 file(REMOVE_RECURSE "${prefix}" "${build}")
 
-execute_process(COMMAND "${CMAKE_COMMAND}" --install "${VKEXEC_BINARY_DIR}" --prefix "${prefix}" COMMAND_ERROR_IS_FATAL
-                        ANY)
+execute_process(
+  COMMAND "${CMAKE_COMMAND}" --install "${VKEXEC_BINARY_DIR}" --prefix "${prefix}" --config "${VKEXEC_CONFIG}"
+  COMMAND_ERROR_IS_FATAL ANY)
 
 set(consumer_configure_args
     "-S"
@@ -41,7 +56,7 @@ set(consumer_configure_args
     "-B"
     "${build}"
     "-DCMAKE_PREFIX_PATH=${prefix}"
-    "-DCMAKE_BUILD_TYPE=Release")
+    "-DCMAKE_BUILD_TYPE=${VKEXEC_CONFIG}")
 
 vkexec_read_cache_entry("${parent_cache}" "Vulkan_INCLUDE_DIR" _vulkan_include_dir)
 vkexec_read_cache_entry("${parent_cache}" "Vulkan_LIBRARY" _vulkan_library)
@@ -54,4 +69,5 @@ endif()
 
 execute_process(COMMAND "${CMAKE_COMMAND}" ${consumer_configure_args} COMMAND_ERROR_IS_FATAL ANY)
 
-execute_process(COMMAND "${CMAKE_COMMAND}" --build "${build}" --config Release --parallel 10 COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND "${CMAKE_COMMAND}" --build "${build}" --config "${VKEXEC_CONFIG}" --parallel 10
+                        COMMAND_ERROR_IS_FATAL ANY)
