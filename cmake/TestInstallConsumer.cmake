8fd8a73005e576d3b56f5b9734f228b617268cde
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-09-24 06:41:15 +0900

    fix: make install consumer use external Boost package

--- cmake/TestInstallConsumer.cmake
+++ cmake/TestInstallConsumer.cmake
@@ -2,7 +2,9 @@ cmake_minimum_required(VERSION 3.29)
 
 if(NOT DEFINED VKEXEC_SOURCE_DIR
    OR NOT DEFINED VKEXEC_BINARY_DIR
-   OR NOT DEFINED CONSUMER_SOURCE_DIR)
+   OR NOT DEFINED CONSUMER_SOURCE_DIR
+   OR NOT DEFINED VKEXEC_BOOST_SOURCE_DIR
+   OR NOT DEFINED VKEXEC_GENERATOR)
   message(FATAL_ERROR "Required test paths were not provided")
 endif()
 
@@ -28,6 +30,8 @@ endfunction()
 
 set(prefix "${VKEXEC_BINARY_DIR}/install-consumer/prefix")
 set(build "${VKEXEC_BINARY_DIR}/install-consumer/build")
+set(boost_prefix "${VKEXEC_BINARY_DIR}/install-consumer/boost-prefix")
+set(boost_build "${VKEXEC_BINARY_DIR}/install-consumer/boost-build")
 set(parent_cache "${VKEXEC_BINARY_DIR}/CMakeCache.txt")
 
 # Multi-config generators place archives under $<CONFIG>/; cmake --install must
@@ -46,18 +50,103 @@ if(NOT DEFINED VKEXEC_CONFIG
   endif()
 endif()
 
-file(REMOVE_RECURSE "${prefix}" "${build}")
+file(REMOVE_RECURSE "${prefix}" "${build}" "${boost_prefix}" "${boost_build}")
 
-execute_process(COMMAND "${CMAKE_COMMAND}" --install "${VKEXEC_BINARY_DIR}" --prefix "${prefix}" --config
-                        "${VKEXEC_CONFIG}" COMMAND_ERROR_IS_FATAL ANY)
+set(generator_args "-G" "${VKEXEC_GENERATOR}")
+if(DEFINED VKEXEC_GENERATOR_PLATFORM
+   AND NOT "${VKEXEC_GENERATOR_PLATFORM}" STREQUAL "")
+  list(APPEND generator_args "-A" "${VKEXEC_GENERATOR_PLATFORM}")
+endif()
+if(DEFINED VKEXEC_GENERATOR_TOOLSET
+   AND NOT "${VKEXEC_GENERATOR_TOOLSET}" STREQUAL "")
+  list(APPEND generator_args "-T" "${VKEXEC_GENERATOR_TOOLSET}")
+endif()
+
+vkexec_read_cache_entry(
+  "${parent_cache}"
+  "CMAKE_CONFIGURATION_TYPES"
+  _configuration_types)
+vkexec_read_cache_entry(
+  "${parent_cache}"
+  "CMAKE_TOOLCHAIN_FILE"
+  _toolchain_file)
+vkexec_read_cache_entry(
+  "${parent_cache}"
+  "CMAKE_GENERATOR_INSTANCE"
+  _generator_instance)
+vkexec_read_cache_entry(
+  "${parent_cache}"
+  "CMAKE_CXX_COMPILER"
+  _cxx_compiler)
+
+if(_toolchain_file AND NOT IS_ABSOLUTE "${_toolchain_file}")
+  if(EXISTS "${VKEXEC_BINARY_DIR}/${_toolchain_file}")
+    cmake_path(
+      ABSOLUTE_PATH _toolchain_file
+      BASE_DIRECTORY "${VKEXEC_BINARY_DIR}")
+  elseif(EXISTS "${VKEXEC_SOURCE_DIR}/${_toolchain_file}")
+    cmake_path(
+      ABSOLUTE_PATH _toolchain_file
+      BASE_DIRECTORY "${VKEXEC_SOURCE_DIR}")
+  else()
+    message(
+      FATAL_ERROR
+      "Could not resolve parent CMAKE_TOOLCHAIN_FILE: ${_toolchain_file}")
+  endif()
+endif()
+
+set(boost_configure_args
+    "-S"
+    "${VKEXEC_BOOST_SOURCE_DIR}"
+    "-B"
+    "${boost_build}"
+    ${generator_args}
+    "-DBOOST_ENABLE_CMAKE=ON"
+    "-DBOOST_INCLUDE_LIBRARIES=system"
+    "-DBOOST_SKIP_INSTALL_RULES=OFF"
+    "-DBUILD_TESTING=OFF"
+    "-DCMAKE_INSTALL_PREFIX=${boost_prefix}")
 
 set(consumer_configure_args
     "-S"
     "${CONSUMER_SOURCE_DIR}"
     "-B"
     "${build}"
+    ${generator_args}
     "-DCMAKE_PREFIX_PATH=${prefix}"
-    "-DCMAKE_BUILD_TYPE=${VKEXEC_CONFIG}")
+    "-DBoost_ROOT=${boost_prefix}")
+
+if(NOT _configuration_types)
+  list(APPEND boost_configure_args "-DCMAKE_BUILD_TYPE=${VKEXEC_CONFIG}")
+  list(APPEND consumer_configure_args "-DCMAKE_BUILD_TYPE=${VKEXEC_CONFIG}")
+endif()
+
+if(_toolchain_file)
+  list(APPEND boost_configure_args "-DCMAKE_TOOLCHAIN_FILE=${_toolchain_file}")
+  list(APPEND consumer_configure_args "-DCMAKE_TOOLCHAIN_FILE=${_toolchain_file}")
+endif()
+
+if(_generator_instance)
+  list(APPEND boost_configure_args "-DCMAKE_GENERATOR_INSTANCE=${_generator_instance}")
+  list(APPEND consumer_configure_args "-DCMAKE_GENERATOR_INSTANCE=${_generator_instance}")
+endif()
+
+if(NOT _toolchain_file
+   AND _cxx_compiler
+   AND NOT VKEXEC_GENERATOR MATCHES "Visual Studio"
+   AND NOT VKEXEC_GENERATOR MATCHES "Xcode")
+  list(APPEND boost_configure_args "-DCMAKE_CXX_COMPILER=${_cxx_compiler}")
+  list(APPEND consumer_configure_args "-DCMAKE_CXX_COMPILER=${_cxx_compiler}")
+endif()
+
+execute_process(COMMAND "${CMAKE_COMMAND}" ${boost_configure_args} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND "${CMAKE_COMMAND}" --build "${boost_build}" --config "${VKEXEC_CONFIG}" --parallel 10
+                COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND "${CMAKE_COMMAND}" --install "${boost_build}" --config "${VKEXEC_CONFIG}"
+                COMMAND_ERROR_IS_FATAL ANY)
+
+execute_process(COMMAND "${CMAKE_COMMAND}" --install "${VKEXEC_BINARY_DIR}" --prefix "${prefix}" --config
+                        "${VKEXEC_CONFIG}" COMMAND_ERROR_IS_FATAL ANY)
 
 vkexec_read_cache_entry("${parent_cache}" "Vulkan_INCLUDE_DIR" _vulkan_include_dir)
 vkexec_read_cache_entry("${parent_cache}" "Vulkan_LIBRARY" _vulkan_library)
